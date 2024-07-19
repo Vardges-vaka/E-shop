7bb470c114f6022ba1a06d213cd6e537efa36fc1
Vardges <vp@thecocktailtree.com>
2024-07-19 09:07:59 +0400

    Updated JavaScript  and other files

--- 1.js
+++ 1.js
@@ -1,131 +1,54 @@
+// import
+import { apiCall } from "./ApiCall.js";
+import { showCartItems } from "./displayFunction.js";
+import { createItem } from "./displayFunction.js";
+
+// export
+export let card_container = document.getElementById("items");
+export let modal_content = document.querySelector(".modal-content");
+export let shoppingCart = [];
+
 let cartIcon = document.getElementById("cart_id");
 let overlay = document.querySelector(".overlay");
-let card_container = document.getElementById("items");
-let modal_content = document.querySelector(".modal-content");
-let shoppingCart = []; 
 
-
-cartIcon.addEventListener("click", () => { 
+cartIcon.addEventListener("click", () => {
   overlay.style.display = "block";
 });
 
-modal_content.addEventListener("click", (e)=>{
+modal_content.addEventListener("click", (e) => {
   e.stopPropagation();
+});
 
-})
-
-
-
-overlay.addEventListener("click", () => { 
+overlay.addEventListener("click", () => {
   overlay.style.display = "none";
 });
 
-
-let apiCall=()=>{
-  let promise = new Promise ((resolve, reject) =>{
-    let data = fetch("https://fakestoreapi.com/products").then((res)=>res.json());
-        if (data){
-      resolve(data);
-    } else{
-      reject();
-    }
+export let item_adding_to_the_cart = (product) => {
+  let isItemInCart = shoppingCart.find((item) => {
+    return product.id == item.id;
   });
-return promise;
+  if (!isItemInCart) {
+    shoppingCart.push({ ...product, item_QNT: 1 });
+    showCartItems();
+    // alert("You have already added this item in the cartIcon")
+  } else {
+    shoppingCart.map((item) => {
+      if ((item.id = product.id)) {
+        return { ...item, item_QNT: item.item_QNT++ };
+      } else {
+        return item;
+      }
+    });
+  }
+  console.log(shoppingCart, "adding an item");
 };
 
-window.onload=() =>  {
+window.onload = () => {
   apiCall()
-  .then((data)=> {
-    data.forEach((product) => {   
-    console.log(product);
-
-      // creating a container for a single product
-    let card = document.createElement("div");
-    card.classList = "card";
-    // creaating the image container for the caed-container 
-    let imgeContainer = document.createElement("div");
-    // imgeContainer.style.width="";
-    // imgeContainer.style.height="";
-    card.appendChild(imgeContainer);
-    // creaating the product's image for its container 
-    let productImage = document.createElement("img")
-    productImage.id = "img";
-    productImage.src = product.image;
-    imgeContainer.appendChild(productImage);
-    // creaating the product's name container
-    let productNameContainer = document.createElement("div");
-    productNameContainer.classList = "card-name";
-    card.appendChild(productNameContainer);
-    // creaating the product's name element
-    let productName = document.createElement("p");
-    productName.classList = "heado";
-    productName.textContent= product.title;
-    productNameContainer.appendChild(productName);
-    // creaating the product's name price
-    let productPrice = document.createElement("p");
-    productPrice.classList = "heado";
-    productPrice.textContent= product.price +"aed";
-    productNameContainer.appendChild(productPrice);
-    // creating a container for Raiting Icons
-    let raitingContainer = document.createElement("div");
-    raitingContainer.classList = "stars-icon";
-    card.appendChild(raitingContainer);
-    // creating an icons for Raitings
-    // ????????????????????????????????????????????????????
-    let raitingIcon1 = document.createElement("img");
-    raitingIcon1.src = "./images/star.png"; 
-    raitingContainer.appendChild(raitingIcon1);
-    
-    let raitingIcon2 = document.createElement("img");
-    raitingIcon2.src = "./images/star.png"; 
-    raitingContainer.appendChild(raitingIcon2);
-
-    let raitingIcon3 = document.createElement("img");
-    raitingIcon3.src = "./images/star.png"; 
-    raitingContainer.appendChild(raitingIcon3);
-
-    let raitingIcon4 = document.createElement("img");
-    raitingIcon4.src = "./images/star.png"; 
-    raitingContainer.appendChild(raitingIcon4);
-
-    let raitingIcon5 = document.createElement("img");
-    raitingIcon5.src = "./images/star.png"; 
-    raitingContainer.appendChild(raitingIcon5);
-    // ????????????????????????????????????????????????????
-    // creating a text for raiting count
-    let raitingQNT = document.createElement("p");
-    // raitingQNT.classList = "";
-    raitingQNT.textContent = product.rating.count; 
-    raitingContainer.appendChild(raitingQNT);
-    // creating a text for raiting count
-    let addToTheCardBtn = document.createElement("button");
-    addToTheCardBtn.textContent="Add to Your Card";
-    card.appendChild(addToTheCardBtn);
-    addToTheCardBtn.addEventListener("click", () => {
-      shoppingCart.push(product);
-      let clonedCard = card.cloneNode(true);
-      let removeBtn = document.createElement("button");
-      removeBtn.classList = "remove-btn";
-      removeBtn.textContent = "remove";
-      clonedCard.appendChild(removeBtn);
-      removeBtn.addEventListener ("click", () => {
-        modal_content.removeChild(clonedCard);
-// How to remove the item from the? shoppingCart array????
-
-
+    .then((data) => {
+      data.forEach((product) => {
+        createItem(product);
       });
-      modal_content.appendChild(clonedCard);
-  });
-
-
-
-    
-    card_container.appendChild(card);
-
-    // console.log(data, "product");
-  })
-  
-  })
-   .catch(()=> alert ("everything is sold out, new items comming soon!"));
+    })
+    .catch(() => alert("everything is sold out, new items comming soon!"));
 };
-
---
+++ displayFunction.js
@@ -0,0 +1,121 @@
+import { item_adding_to_the_cart } from "./1.js";
+import { modal_content } from "./1.js";
+import { shoppingCart } from "./1.js";
+import { card_container } from "./1.js";
+
+export let createItem = (product) => {
+  let { image, title, price } = product;
+  // creating a container for a single product
+  let card = document.createElement("div");
+  card.classList = "card";
+  // creaating the image container for the caed-container
+  let imgeContainer = document.createElement("div");
+  card.appendChild(imgeContainer);
+  // creaating the product's image for its container
+  let productImage = document.createElement("img");
+  productImage.id = "img";
+  productImage.src = image;
+  imgeContainer.appendChild(productImage);
+  // creaating the product's name container
+  let productNameContainer = document.createElement("div");
+  productNameContainer.classList = "card-name";
+  card.appendChild(productNameContainer);
+  // creaating the product's name element
+  let productName = document.createElement("p");
+  productName.classList = "heado";
+  productName.textContent = title;
+  productNameContainer.appendChild(productName);
+  // creaating the product's name price
+  let productPrice = document.createElement("p");
+  productPrice.classList = "heado";
+  productPrice.textContent = price + "aed";
+  productNameContainer.appendChild(productPrice);
+  // creating a container for Raiting Icons
+  let raitingContainer = document.createElement("div");
+  raitingContainer.classList = "stars-icon";
+  card.appendChild(raitingContainer);
+  // creating an icons for Raitings
+  for (let i = 1; i < product.rating.rate; i++) {
+    let raitingIcon1 = document.createElement("img");
+    raitingIcon1.src = "./images/star.png";
+    raitingContainer.appendChild(raitingIcon1);
+  }
+  // creating a text for raiting count
+  let raitingQNT = document.createElement("p");
+  // raitingQNT.classList = "";
+  raitingQNT.textContent = product.rating.count;
+  raitingContainer.appendChild(raitingQNT);
+  // creating a text for raiting count
+  let addToTheCardBtn = document.createElement("button");
+  addToTheCardBtn.textContent = "Add to Your Card";
+  card.appendChild(addToTheCardBtn);
+  addToTheCardBtn.addEventListener("click", () => {
+    item_adding_to_the_cart(product);
+  });
+  card_container.appendChild(card);
+};
+export let showCartItems = () => {
+  modal_content.innerHTML = "";
+  shoppingCart.forEach((item) => {
+    let { image, title, price } = item;
+    let cartItem = document.createElement("div");
+    let cart_img_container = document.createElement("div");
+    let cart_img = document.createElement("img");
+    let title_price_container = document.createElement("div");
+    let cartTitle = document.createElement("p");
+    let cartPrice = document.createElement("p");
+    let removeCartBTN_container = document.createElement("div");
+
+    let removeCartBTN = document.createElement("button");
+    // giving classesNames
+    cartItem.classList = "cart-sng-item";
+    cart_img_container.classList = "cart-img-container";
+    title_price_container.classList = "title-price-container";
+    cartTitle.classList = "cart-Title";
+    cartPrice.classList = "cart-Price";
+    removeCartBTN_container.classList = "remove-Cart-BTN-container";
+    removeCartBTN.classList = "remove-Cart-BTN";
+    // adding Content
+    removeCartBTN.textContent = "Remove";
+    cart_img.src = image;
+    cartTitle.textContent = title;
+    cartPrice.textContent = price + " aed";
+
+    cart_img_container.appendChild(cart_img);
+    cartItem.appendChild(cart_img_container);
+    cartItem.appendChild(title_price_container);
+    title_price_container.appendChild(cartTitle);
+    title_price_container.appendChild(cartPrice);
+    cartItem.appendChild(removeCartBTN_container);
+    removeCartBTN_container.appendChild(removeCartBTN);
+
+    removeCartBTN.addEventListener("click", () => {
+      item = shoppingCart
+        .map((cartItem) => {
+          if (cartItem.id === item.id) {
+            return { ...cartItem, item_QNT: cartItem.item_QNT-- };
+          }
+          return cartItem;
+        })
+        .filter((cartItem) => cartItem.item_QNT > 0);
+
+      showCartItems();
+      console.log(shoppingCart, "Updated shopping cart after removal:");
+    });
+    modal_content.appendChild(cartItem);
+  });
+};
+
+// let updatedCAart=shoppingCart.filter((cartItem)=>{return cartItem.id!=item.id})
+// modal_content.removeChild(cartItem);
+
+// removeCartBTN.addEventListener("click", () => {
+//   shoppingCart = shoppingCart.map((cartItem) => {
+//     if (cartItem.id === item.id) {
+//       return { ...cartItem, item_QNT: cartItem.item_QNT - 1 };
+//     }
+//     return cartItem;
+//   }).filter((cartItem) => cartItem.item_QNT > 0);
+//   showCartItems();
+//   console.log(shoppingCart, item, "after delete");
+// });
